057c5752032d012c9a6ab9f274ef37f45d60521e
ayush460 <90319070+ayush460@users.noreply.github.com>
2023-04-15 21:05:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function changeColor() {
+	var blockId = document.getElementById("block_id").value;
+	var color = document.getElementById("color_id").value;
+	document.getElementById(blockId).style.backgroundColor = color;
+}
+
+function resetColor() {
+	var blocks = document.getElementsByClassName("grid-item");
+	for (var i = 0; i < blocks.length; i++) {
+		blocks[i].style.backgroundColor = "transparent";
+	}
+}
